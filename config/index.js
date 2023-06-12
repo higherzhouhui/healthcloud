c9ab0931579c982b3f9533f99f3330f6bcadc764
liujiahuan <liujiahuan@zbj.com>
2023-06-12 21:23:59 +0800

    打包

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
 // 请求的基本路径
 
 const baseURL = process.env.NODE_ENV === "development" ?
-(window ? '/api' : "https://338v828404.goho.co")://测试环境(请求接口的代理路径)
-"https://338v828404.goho.co";//上线环境
+(window ? '/api' : "http://api.zhengtaixinnengyuan.com")://测试环境(请求接口的代理路径)
+"http://api.zhengtaixinnengyuan.com";//上线环境
  
 export {baseURL}
