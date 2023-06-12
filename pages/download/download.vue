7d610800c4cea539319d92ab19993d9e9f9eeda5
peggy <258409778@qq.com>
2023-06-12 17:48:32 +0800

    1

--- pages/download/download.vue
+++ pages/download/download.vue
@@ -1,84 +1,105 @@
 <template>
 	<view class="main">
 		<view class="bottom">
-		  <a download="正泰新能源.apk " :href="versionInfo.downUrl" class="download">立即下载</a>
-		  <view class="version">版本号:{{versionInfo.version}}</view>
+			<view class="img-box">
+				<image src="../../static/download/logo.png" class="img"></image> 
+			</view>
+			
+			<a download="正泰新能源.apk " :href="versionInfo.downUrl" class="download">立即下载</a>
+				<!-- <view class="version">版本号:{{versionInfo.version}}</view> -->
 		</view>
 	</view>
 </template>
 
 <script>
-	import {getVersionRequest} from "@/api/user.js"
-	export default {
-		data() {
-			return {
-				versionInfo: {}
+import { getVersionRequest } from "@/api/user.js"
+export default {
+	data() {
+		return {
+			versionInfo: {}
+		}
+	},
+	onLoad(options) {
+		if (!options.useH5) {
+			this.int()
+		}
+		this.getVersion()
+	},
+	methods: {
+		int() {
+			// #ifdef H5
+			var a = document.getElementsByClassName('uni-page-head-hd')
+			if (a.length) {
+				a[0].style.display = 'none';
 			}
+			// #endif
 		},
-		onLoad(options) {
-			if (!options.useH5) {
-				this.int()
-			}
-			this.getVersion()
-		},
-		methods: {
-			int(){
-				// #ifdef H5
-				var a = document.getElementsByClassName('uni-page-head-hd')
-				if (a.length) {
-					a[0].style.display = 'none';
+		getVersion() {
+			getVersionRequest({ deviceType: 1 }).then(res => {
+				if (res.code === 200) {
+					this.versionInfo = res.data
 				}
-				// #endif
-			},
-			getVersion() {
-				getVersionRequest({ deviceType:1 }).then(res => {
-					if (res.code === 200) {
-						this.versionInfo = res.data
-					}
-				})
-			}
+			})
 		}
 	}
+}
 </script>
 
 <style scoped lang="scss">
-  .main {
-    width: 100%;
-    height: calc(100vh - 44px);
-    background-image: url('../../static/start.png');
-    background-size: 100% 100%;
-    background-repeat: no-repeat;
-  }
-  .logo {
-    width: 200px;
-    height: 165px;
-  } 
-  .bottom {
-	  display: flex;
-	  flex-direction: column;
-	  align-items: center;
-	  position: fixed;
-	  bottom: 80px;
-	  width: 100%;
-  }
-  .download {
-    width: 256px;
-    height: 50px;
-    background: linear-gradient(90deg, #FFC42E 0%, #FF822E 100%);
-    border-radius: 28px 28px 28px 28px;
-    opacity: 1;
-    text-decoration: none;
-    text-align: center;
-    line-height: 50px;
-    color: #fff;
-    font-size: 18px;
-
-  }
-  .version {
-    font-size: 15px;
-    font-family: PingFang SC-Regular, PingFang SC;
-    font-weight: 400;
-    color: #FFFFFF;
-		margin-top: 12px;
+.main {
+	width: 100%;
+	height: calc(100vh - 44px);
+	background-image: url('../../static/download/bg.png');
+	background-size: cover;
+	background-repeat: no-repeat;
+	text-align: center;
+}
+.img-box{
+	margin: 0 auto 105px;
+	position: relative;
+	width: 58.6%;
+	padding-bottom: 34.5%;
+	.img{
+		left: 0;
+		top: 0;
+		position: absolute;
+		width: 100%;
+		height: 100%;
 	}
-</style>
+}
+.logo {
+	width: 200px;
+	height: 165px;
+}
+
+.bottom {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	position: fixed;
+	top: 50%;
+	transform: translateY(-50%);
+	width: 100%;
+}
+
+.download {
+	width: 256px;
+	height: 50px;
+	background: linear-gradient(90deg, #F57E6A 0%, #FE1E27 100%);
+	border-radius: 28px 28px 28px 28px;
+	opacity: 1;
+	text-decoration: none;
+	text-align: center;
+	line-height: 50px;
+	color: #fff;
+	font-size: 18px;
+
+}
+
+.version {
+	font-size: 15px;
+	font-family: PingFang SC-Regular, PingFang SC;
+	font-weight: 400;
+	color: #FFFFFF;
+	margin-top: 12px;
+}</style>
