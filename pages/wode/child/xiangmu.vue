62ed6cd2caa6802b1f6dc804181530fbebb7633d
liujiahuan <liujiahuan@zbj.com>
2023-06-17 01:56:32 +0800

    1

--- pages/wode/child/xiangmu.vue
+++ pages/wode/child/xiangmu.vue
@@ -15,7 +15,7 @@
             </view>
             <view class="botton-info">
                 <view class="in-item">
-                    <view class="value">{{ item.chntSubsidy }}</view>
+                    <view class="value">{{ item.projectType == 1 ? item.chntSubsidy : item.equityValue }}</view>
                     <view class="txt">{{ item.projectType == 1 ? '奖励数字人民币' : '股权总价值' }}</view>
                 </view>
                 <view class="in-item"  v-if="item.projectType == 1">
