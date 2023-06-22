a8863d9c7336de686fd7b1b1d2ec5ffff867ae3a
higherzhouhui <782492184@qq.com>
2023-06-22 10:37:34 +0800

    更好api地址

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 //在config/index.js
- const LINK = "http://web.jianxiangyunbao.com/"
+ const LINK = "http://web.jianxiangyunbao.cc/"
 // 请求的基本路径
-const URL = "http://api.jianxiangyunbao.com"
+const URL = "http://api.jianxiangyunbao.cc"
 const baseURL = process.env.NODE_ENV === "development" ?
-(window ? '/api' : URL)://测试环境(请求接口的代理路径)
+(window ? URL || '/api' : URL)://测试环境(请求接口的代理路径)
 URL;//上线环境
  
 export {baseURL, URL, LINK}
