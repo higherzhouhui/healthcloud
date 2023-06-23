1ff4ba5c273151ae8e63ab5ab7d7dc932dde494f
higherzhouhui <782492184@qq.com>
2023-06-23 14:20:24 +0800

    update

--- App.vue
+++ App.vue
@@ -1,6 +1,5 @@
 <script>
-	import appUpdate from 'common/util/appUpdate.js'
-
+	import appUpdate from '@/common/util/appUpdate.js'
 	export default {
 		onLaunch: function() {
 			// #ifdef APP-PLUS
@@ -19,8 +18,8 @@
 <style lang="scss">
 	/*每个页面公共css */
 	@import '@/uni_modules/uni-scss/index.scss';
-	/* #ifndef APP-NVUE */
 	@import '@/static/customicons.scss';
+	/* #ifndef APP-NVUE */
 	// 设置整个项目的背景色
 	page {
 		background-color: #f5f5f5;
