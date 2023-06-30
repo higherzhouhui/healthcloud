d18b7d94301ed88fdcbbc41a91645bf8b8f032f9
liujiahuan <liujiahuan@zbj.com>
2023-07-01 02:40:02 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	  devServer: {
 	    proxy: {
 	     '/api': {
-	             target: 'https://api.jianxiangyunbao.cc',
+	             target: 'https://338v828404.goho.co',
 	             changeOrigin: true, 
 	                 pathRewrite: {
 	                 "^/api": ""
