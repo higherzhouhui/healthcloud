4e051540631e74ffb66a3ef183eac99bb5dfddbb
higherzhouhui <782492184@qq.com>
2023-07-02 17:14:31 +0800

    fix

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 //在config/index.js
  const LINK = "http://web.jianxiangyunbao.cc/"
 // 请求的基本路径
-const URL = "http://192.168.2.166:8000"
+const URL = "https://api.jianxiangyunbao.cc"
 const baseURL = process.env.NODE_ENV === "development" ?
-(window ? URL : URL)://测试环境(请求接口的代理路径)
+(window ? '/api' : URL)://测试环境(请求接口的代理路径)
 URL;//上线环境
  
 export {baseURL, URL, LINK}
